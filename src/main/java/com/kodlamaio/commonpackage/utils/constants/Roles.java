b77e973ba7871ce6e303cd54914674feb936cf32
ismailatac <atac571@outlook.com>
2023-06-07 14:43:53 +0300

    add roles

--- src/main/java/com/kodlamaio/commonpackage/utils/constants/Roles.java
+++ src/main/java/com/kodlamaio/commonpackage/utils/constants/Roles.java
@@ -2,4 +2,7 @@ package com.kodlamaio.commonpackage.utils.constants;
 
 public class Roles {
     public static final String AdminAndUser = "hasRole('admin') and hasRole('user')";
+    public static final String Admin = "hasRole('admin')";
+    public static final String User = "hasRole('user')";
+    public static final String AdminOrUser = "hasRole('admin') or hasRole('user')";
 }
